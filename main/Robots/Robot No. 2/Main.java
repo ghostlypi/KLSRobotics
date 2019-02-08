a6628c16a06214e67618135a90f4ea9b9f18aa51
the-inhuman-account <31903083+the-inhuman-account@users.noreply.github.com>
2019-02-07 19:32:02 -0800

    Update Main.java

--- main/Robots/Robot No. 2/Main.java
+++ main/Robots/Robot No. 2/Main.java
@@ -1,29 +1,243 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot;
 
-import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.TimedRobot;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj.CameraServer;
+import edu.wpi.cscore.CvSink;
+import edu.wpi.cscore.UsbCamera;
+import edu.wpi.first.wpilibj.Encoder;
+import org.opencv.core.Mat;
 
 /**
- * Do NOT add any static variables to this class, or any initialization at all.
- * Unless you know what you are doing, do not modify this file except to
- * change the parameter class to the startRobot call.
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the IterativeRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
  */
-public final class Main {
-  private Main() {
-  }
+public class Robot extends TimedRobot {
+	// proportional integral derivative (courtesy of NV)
+	int P, I, D = 1;	
+	int integral, previous_error, setpoint = 0;
+	int switchSide = 0;
+	int driverStation = 1;
+	int count = 0;
+	double proportional = 0.3;
+	double Integral = 0.2;
+	double goal = 0;
+	double gripperMovement;
 
-  /**
-   * Main initialization function. Do not perform any initialization here.
-   *
-   * <p>If you change your main robot class, change the parameter type.
-   */
-  public static void main(String... args) {
-    RobotBase.startRobot(Robot::new);
-  }
+	double error;
+	double runningSpeed = 0;
+	int integralTracker = 0;
+	double armSpeed;
+
+	boolean joystickGripIn;
+	boolean joystickGripOut;
+	boolean buttonGripperIntake;
+	boolean buttonGripperRelease;
+	
+	//time
+	long start;
+	//Motors
+	RobotDrive myDrive;
+	Spark lArmSpark;
+	Spark rArmSpark;
+	TalonSRX leftTalon;
+	TalonSRX rightTalon;
+
+	//Encoder
+	Encoder armEncoder;
+
+	//Stored Values
+	double joystickLValue;
+	double joystickRValue;
+	double joystickArmValue;
+	double joystickWheelSpeedValue;
+
+	//Camera
+	private UsbCamera camera;
+	private CvSink sink;
+	private Mat source;
+
+	//PID Vars
+	/*
+	* Experimental code. Waiting for complete module
+	* int P, I, D = 1;
+	* int integral, previous_error, setpoint = 0;
+	* double proportional = 0.3;
+	* double Integral = 0.2;
+	* double goal = 0;
+	* double error;
+	* double runningSpeed = 0;
+	* int integralTracker = 0;
+	* double armSpeed;
+	*/
+	//Joysticks
+	Joystick joystick0 = new Joystick(0);
+	Joystick joystick1 = new Joystick(1);
+
+	/**
+	 * This function is run when the robot is first started up and should be used
+	 * for any initialization code.
+	 */
+	@Override
+	public void robotInit() {
+		//Drive Train
+		myDrive = new RobotDrive(0, 1);
+		//Arm
+		lArmSpark = new Spark(2);
+		rArmSpark = new Spark(3);
+		//Intake
+		leftTalon = new TalonSRX(0);
+		rightTalon = new TalonSRX(1);
+		//encoder stuff
+		armEncoder  = new Encoder(0, 1, true, Encoder.EncodingType.k4X);
+		armEncoder.setMaxPeriod(0.05);
+		armEncoder.setMinRate(10);
+		armEncoder.setDistancePerPulse(2.8125);
+		armEncoder.setSamplesToAverage(10);
+		armEncoder.reset();
+		//Camera
+		camera = CameraServer.getInstance().startAutomaticCapture();
+		camera.setResolution(840,680);
+		sink = CameraServer.getInstance().getVideo();
+		source = new Mat();
+	}
+ 
+	/**
+	 * This autonomous (along with the chooser code above) shows how to select
+	 * between different autonomous modes using the dashboard. The sendable chooser
+	 * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
+	 * remove all of the chooser code and uncomment the getString line to get the
+	 * auto name from the text box below the Gyro
+	 *
+	 * You can add additional auto modes by adding additional comparisons to the
+	 * switch structure below with additional strings. If using the SendableChooser
+	 * make sure to add them to the chooser code above as well.
+	 */
+	@Override
+	public void autonomousInit() {
+		start = System.currentTimeMillis();
+	}
+	/**
+	 * This function is called periodically during autonomous
+	 */
+	@Override
+	
+	public void autonomousPeriodic() {
+	}	
+
+	/**
+	 * This function is called periodically during operator control
+	 */
+
+	public double roboRIOCameraAlignmentSystemGetAngleToTurn() {
+		sink.grabFrame(source);
+		MyVector choice = ProcessImage.processImage(source);
+		return choice.getAngle();
+	}
+
+	@Override
+	public void teleopPeriodic() {
+		// PID
+		if(joystick0.getRawButton(1))
+		{
+			goal = 1.0;
+			integralTracker = 0;
+		}
+		else if(joystick0.getRawButton(2))
+		{
+			goal = 0.0;
+			integralTracker = 0;
+		}
+		else if(joystick0.getRawButton(3))
+		{
+			goal = 0.5;
+			integralTracker = 0;
+		}
+
+		// Arm speed.
+		armSpeed = joystick0.getRawAxis(1);
+		armSpark1.set(armSpeed);
+		armSpark2.set(armSpeed);
+		
+		// encoder with PID.
+		count = armEncoder.get();
+		eMeasure = armEncoder.getDistance();
+		System.out.println(eMeasure);
+		error = (360 * goal) - eMeasure;
+		if(count % 5 == 0)
+		{
+			integralTracker += error;
+		}
+		runningSpeed = ((error * proportional)/360) + ((integralTracker * Integral)/360);
+
+		//Drive Train
+		joystickLValue = joystick0.getRawAxis(1);
+		joystickRValue = joystick0.getRawAxis(1)*0.913;
+
+		// Intake.
+		gripperMovement = joystick1.getRawAxis(2) - joystick1.getRawAxis(3);
+		leftTalon.set(ControlMode.PercentOutput, gripperMovement);
+		rightTalon.set(ControlMode.PercentOutput, -gripperMovement);
+
+		// tankDrive.
+		if(joystick0.getRawAxis(2) < -0.1)
+    	{
+    		joystickLValue -= joystick0.getRawAxis(2);
+    		joystickRValue += joystick0.getRawAxis(2);
+    	}
+    	if(joystick0.getRawAxis(2) > 0.1)
+    	{
+    		joystickLValue -= joystick0.getRawAxis(2);
+    		joystickRValue += joystick0.getRawAxis(2);
+    	}
+
+		if(joystick0.getRawAxis(2) < -0.1)
+    	{
+    		joystickLValue -= joystick0.getRawAxis(2);
+    		joystickRValue += joystick0.getRawAxis(2);
+    	}
+    	if(joystick0.getRawAxis(2) > 0.1)
+    	{
+    		joystickLValue -= joystick0.getRawAxis(2);
+    		joystickRValue += joystick0.getRawAxis(2);
+    	}
+
+		myDrive.tankDrive(joystickLValue, joystickRValue);
+
+		//Arm
+		joystickArmValue = joystick1.getRawAxis(1);
+		lArmSpark.set(joystickArmValue);
+		rArmSpark.set(joystickArmValue);
+
+
+		//intake
+		// if(joystick1.getRawButton(1)){
+		// 	joystickWheelSpeedValue = 0.85;
+		// }else if(joystick1.getRawButton(2)){
+		// 	joystickWheelSpeedValue = -0.85;
+		// }else{
+		// 	joystickWheelSpeedValue = 0;
+		// }
+		// leftTalon.set(ControlMode.PercentOutput,joystickWheelSpeedValue);
+		// rightTalon.set(ControlMode.PercentOutput,joystickWheelSpeedValue);
+
+		//Camera
+		double angle = roboRIOCameraAlignmentSystemGetAngleToTurn();
+		System.out.println(angle);
+	}
+
+	/**
+	 * This function is called periodically during test mode
+	 */
+	@Override
+	public void testPeriodic() {
+	}
 }
