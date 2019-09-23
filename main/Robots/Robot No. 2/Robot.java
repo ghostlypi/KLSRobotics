5152b5e73bc2e810408ace809771c80e06346a4e
ghostlypi <parth.iyer@gmail.com>
2019-09-22 17:12:33 -0700

    Update Robot.java

--- main/Robots/Robot No. 2/Robot.java
+++ main/Robots/Robot No. 2/Robot.java
@@ -102,22 +102,14 @@ public class Robot extends TimedRobot {
 
      public void robotInit() {
 
-	 new Thread(() -> {
-                UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
-                camera.setResolution( 640, 480 );
-                
-                CvSink cvSink = CameraServer.getInstance().getVideo();
-                CvSource outputStream = CameraServer.getInstance().putVideo( "Blur", 640, 480 );
-                
-                Mat source = new Mat();
-                Mat output = new Mat();
-                
-                while( !Thread.interrupted() ) {
-                    cvSink.grabFrame( source );
-                    Imgproc.cvtColor( source, output, Imgproc.COLOR_BGR2GRAY );
-                    outputStream.putFrame( output );
-                }
-            }).start();
+	 Thread t = new Thread(){
+		 
+		 UsbCamera camera = new UsbCamera(“cam0”,0);
+		 camera.setFPS(15);
+		 camera.setResolution(1280, 720);
+		 CameraServer.getInstance().startAutomaticCapture(camera);
+		 
+	 };
 	     
          //Drive Train
 
